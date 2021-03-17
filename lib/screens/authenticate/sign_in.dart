51379f6a049027648429d205947886141e2b0cf3
fviel <nandoviel@hotmail.com>
2021-03-17 07:47:22 -0300

    metade implementação do signin email e pwd

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -16,10 +16,16 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
 
+  //essa globalkey ao form deste widget
+  final _formKey = GlobalKey<FormState>();
+
+  //instancia da classe que criei para tratar os erviço de autenticação
   final AuthService _auth = new AuthService();
-  //text field state
+
+  //variáveis locais do text field state
   String email = '';
   String password = '';
+  String error = '';
 
 
   @override
@@ -44,9 +50,16 @@ class _SignInState extends State<SignIn> {
       body: Container(
           padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
           child: Form(
+            key: _formKey,
             child: Column(children: <Widget>[
               SizedBox(height: 20.0),
-              TextFormField(
+              TextFormField(//validator é a função que valida o campo
+                validator:(val){
+                  if(val.isEmpty){
+                    return 'Informe um email';
+                  }
+                  return null;
+                },
                 onChanged: (val) {
                   setState(() {
                     //mapeia o conteúdo deste campo na var password
@@ -57,7 +70,11 @@ class _SignInState extends State<SignIn> {
               SizedBox(
                 height: 20.0,
               ),
-              TextFormField(obscureText: true, onChanged: (val) {
+              TextFormField(
+                  obscureText: true,
+                  //validator é a função que valida o campo
+                  validator:(val) => val.length<6 ? 'Informe uma senha acima de 6 chars' :  null,
+                  onChanged: (val) {
                 //mapeia o conteúdo deste campo na var password
                 setState(() => password = val);
               }),
@@ -71,18 +88,30 @@ class _SignInState extends State<SignIn> {
                   ),
                 ),
                 onPressed: () async {
-                  dynamic result = await _auth.signInAnon();
-                  if (result == null) {
-                    print('Ocorreu um erro no sign in');
 
+                  //valida o form baseado no seu estado atual
+                  if (_formKey.currentState.validate()) {
+                    // dynamic result = await _auth.signInAnon();
+                    dynamic result = await _auth.registerWithEmailAndPassword(email:email, password:password);
+                    if(result == null){
+                      setState(() => error = 'Falha ao registrar o usuário.');
+
+                    }
+                    //se não falhou na criação do usuário, a Stream de _auth já notificou o Wrapper e já roteou o usuário pra home...
                   } else {
-                    print('Sign in anônimo realizado com sucesso!');
-                    print(result.uid);
-                    print(email);
-                    print(password);
+                    print('form inválido');
                   }
                 },
-              )
+              ),
+              SizedBox(height: 20.0),
+              //exibe a mesg de erro na text abaixo...
+              Text(
+                error,
+                style:TextStyle(
+                  fontSize: 14,
+                  color:Colors.red,
+                ),
+              ),
             ]),
           )),
     );
