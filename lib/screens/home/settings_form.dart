b8f1ce852c46e68f64425dbbe758ebe2fda9072b
fviel <nandoviel@hotmail.com>
2021-03-19 23:41:05 -0300

    Adicionado o slider da cor do café dentro do form do bottomsheet

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cafe/shared/constants.dart';
 
-
 class SettingsForm extends StatefulWidget {
   @override
   _SettingsFormState createState() => _SettingsFormState();
@@ -10,7 +9,7 @@ class SettingsForm extends StatefulWidget {
 class _SettingsFormState extends State<SettingsForm> {
   //essa globalkey ao form deste widget
   final _formKey = GlobalKey<FormState>();
-  final List<String> sugars = ['0','1','2','3','4'];
+  final List<String> sugars = ['0', '1', '2', '3', '4'];
 
   String _currentName;
   String _currentSugars;
@@ -19,42 +18,58 @@ class _SettingsFormState extends State<SettingsForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: _formKey,
-      child: Column(
-        children:<Widget>[
+        key: _formKey,
+        child: Column(children: <Widget>[
           Text('Update your brew settings',
               style: TextStyle(
                 fontSize: 12,
                 color: Colors.brown[800],
               )),
-          SizedBox(height: 20.0,),
+          SizedBox(
+            height: 20.0,
+          ),
           TextFormField(
-              decoration: textInputDecorationFernando.copyWith(hintText: 'Name'),
+              decoration:
+                  textInputDecorationFernando.copyWith(hintText: 'Name'),
               //validator é a função que valida o campo
-              validator: (val) => val.isEmpty ? 'Please inform a name': null,
-              onChanged: (val) { setState(() => _currentName = val); }
-              ),
-          SizedBox(height: 20.0,),
-          //dropdown
+              validator: (val) => val.isEmpty ? 'Please inform a name' : null,
+              onChanged: (val) {
+                setState(() => _currentName = val);
+              }),
+          SizedBox(
+            height: 20.0,
+          ),
           DropdownButtonFormField(
             decoration: textInputDecorationFernando,
             value: _currentSugars ?? '0',
-              onChanged: (val) => setState(() => _currentSugars = val ),
-              items: sugars.map((sugar){
-                return DropdownMenuItem(
-                  value: sugar,
-                  child:Text('$sugar sugars'),
-                );
-              }).toList(),
+            onChanged: (val) => setState(() => _currentSugars = val),
+            items: sugars.map((sugar) {
+              return DropdownMenuItem(
+                value: sugar,
+                child: Text('$sugar sugars'),
+              );
+            }).toList(),
           ),
-          SizedBox(height: 20.0,),
-          //slider
-          //button
-
-
+          SizedBox(
+            height: 20.0,
+          ),
+          Slider(
+            onChanged: (val) => setState(() => _currentStrength = val.round()),
+            value: (_currentStrength ?? 100.00).toDouble(),
+            min: 100.0,
+            max: 900.0,
+            divisions: 8,
+            //passei o valor de strength dentro, pra mudar a cor do componente
+            activeColor: Colors.brown[_currentStrength ?? 100.00],
+            inactiveColor: Colors.brown[100],
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
           RaisedButton(
             color: Colors.pink[400],
-            child: Text('Update',
+            child: Text(
+              'Update',
               style: TextStyle(
                 color: Colors.white,
               ),
@@ -65,10 +80,6 @@ class _SettingsFormState extends State<SettingsForm> {
               print('_currentStrength: ${_currentStrength}');
             },
           ),
-
-
-        ]
-      )
-    );
+        ]));
   }
 }
