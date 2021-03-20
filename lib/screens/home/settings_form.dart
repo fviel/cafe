1c2388fe65091f6582fdd718f8833635dc4a9519
fviel <nandoviel@hotmail.com>
2021-03-19 23:25:01 -0300

    Adicionado o dropdown dentro do form do bottomsheet

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -27,6 +27,46 @@ class _SettingsFormState extends State<SettingsForm> {
                 fontSize: 12,
                 color: Colors.brown[800],
               )),
+          SizedBox(height: 20.0,),
+          TextFormField(
+              decoration: textInputDecorationFernando.copyWith(hintText: 'Name'),
+              //validator é a função que valida o campo
+              validator: (val) => val.isEmpty ? 'Please inform a name': null,
+              onChanged: (val) { setState(() => _currentName = val); }
+              ),
+          SizedBox(height: 20.0,),
+          //dropdown
+          DropdownButtonFormField(
+            decoration: textInputDecorationFernando,
+            value: _currentSugars ?? '0',
+              onChanged: (val) => setState(() => _currentSugars = val ),
+              items: sugars.map((sugar){
+                return DropdownMenuItem(
+                  value: sugar,
+                  child:Text('$sugar sugars'),
+                );
+              }).toList(),
+          ),
+          SizedBox(height: 20.0,),
+          //slider
+          //button
+
+
+          RaisedButton(
+            color: Colors.pink[400],
+            child: Text('Update',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () async {
+              print('_currentName: ${_currentName}');
+              print('_currentSugars: ${_currentSugars}');
+              print('_currentStrength: ${_currentStrength}');
+            },
+          ),
+
+
         ]
       )
     );
